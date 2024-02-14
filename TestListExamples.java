f532379245cc11491c90c526bc52a9bf939a69cd
nk4444 <nkrishnakumar@ucsd.edu>
2024-02-14 12:02:26 -0800

    unfinished, added tests + modified grade.sh

--- TestListExamples.java
+++ TestListExamples.java
@@ -10,12 +10,65 @@ class IsMoon implements StringChecker {
 }
 
 public class TestListExamples {
+
+
+  @Test 
+	public void testFilter() {
+    List<String> input1 = Arrays.asList("a", "ab", "moon", "bcd");
+    List<String> expected1 = Arrays.asList("moon");
+
+    IsMoon contains = new IsMoon();
+
+    List<String> output1 = ListExamples.filter(input1, contains);
+
+
+    List<String> input2 = Arrays.asList("a", "b", "c");
+    List<String> expected2 = Arrays.asList();
+
+    List<String> output2 = ListExamples.filter(input2, contains);
+
+    assertEquals(expected1, output1);
+    assertEquals(expected2, output2);
+  }
+
+  public void testFilterEmpty() {
+    List<String> input1 = Arrays.asList();
+
+    List<String> expected1 = Arrays.asList();
+
+    IsMoon contains = new IsMoon();
+
+
+    List<String> output1 = ListExamples.filter(input1, contains);
+
+
+    assertEquals(expected1, output1);
+  }
+
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
     List<String> right = Arrays.asList("a", "d");
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeLeftEnd() {
+    List<String> left = Arrays.asList("a", "b", "c");
+    List<String> right = Arrays.asList("a", "d");
+    List<String> merged = ListExamples.merge(right, left);
+    List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
+    assertEquals(expected, merged);
+  }
+
+  @Test(timeout = 500)
+  public void testMergeEmpty() {
+    List<String> left = Arrays.asList();
+    List<String> right = Arrays.asList();
+    List<String> merged = ListExamples.merge(left, right);
+    List<String> expected = Arrays.asList();
     assertEquals(expected, merged);
   }
 }
--- grade.sh
+++ grade.sh
@@ -8,9 +8,25 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [ ! -f student-submission/ListExamples.java ]
+then
+    echo "Incorrect file submitted"
+    echo "Score: 0"
+    exit 
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
+cp student-submission/ListExamples.java grading-area
+cp -r lib grading-area
+cp TestListExamples.java grading-area
+cp Server.java grading-area
+cp GradeServer.java grading-area
+
+cd grading-area
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+set +e
+javac -cp $CPATH *.java 
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples 
